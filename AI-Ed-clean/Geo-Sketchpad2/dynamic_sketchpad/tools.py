a008df48a96fce31843d3e1130913243c9779c7b
Nicholas Wang <nyw2006@nyu.edu>
2026-06-02 16:37:10 -0400

    purge

--- AI-Ed-clean/Geo-Sketchpad2/dynamic_sketchpad/tools.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class Tool(StrEnum):
-    CODE_INTERPRETER = "code_interpreter"
-    FILE_SEARCH = "file_search"
-
-    def to_dict(self):
-        return {"type": self.value}
